d4bc3d0b36496254896c0a10e5ac565074c8c734
raj1008-git <macof.rj@gmail.com>
2026-07-31 09:50:54 +0530

    Relational Operstors

---
+++ Fundamentals/Operators/Operators.java
@@ -0,0 +1,8 @@
+class Operators {
+    public static void main(String[] args) {
+        int num = 7;
+        int result = num++;
+        System.out.println(num);
+        System.out.println(result);
+    }
+}
---
+++ Fundamentals/Operators/RelationalOperators.java
@@ -0,0 +1,9 @@
+class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 6;
+
+        boolean result = x >= y;
+        System.out.println(result);
+    }
+}
