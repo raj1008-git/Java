34adf8a9f2fd105ec077a1c1a161a0eda8018859
raj1008-git <macof.rj@gmail.com>
2026-08-02 00:33:21 +0530

    Revised the Do While loops

--- Fundamentals/ConditionalStatements/Iterations.java
+++ Fundamentals/ConditionalStatements/Iterations.java
@@ -1,15 +1,11 @@
 public class Iterations {
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 4) {
-            System.out.println("Hi" + i);
-            int j = 1;
-            while (j <= 3) {
-                System.out.println(" Hello");
+        int i = 5;
 
-                j++;
-            }
+        do {
+            System.out.println("Hi" + i);
             i++;
-        }
+        } while (i <= 4);
+
     }
 }
