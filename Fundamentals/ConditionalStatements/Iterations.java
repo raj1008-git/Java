5cbf6bff49075b220363efe8ff5db0692f100e69
raj1008-git <macof.rj@gmail.com>
2026-08-02 00:22:48 +0530

    Revised the Iterables

---
+++ Fundamentals/ConditionalStatements/Iterations.java
@@ -0,0 +1,15 @@
+public class Iterations {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 4) {
+            System.out.println("Hi" + i);
+            int j = 1;
+            while (j <= 3) {
+                System.out.println(" Hello");
+
+                j++;
+            }
+            i++;
+        }
+    }
+}
