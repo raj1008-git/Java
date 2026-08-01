7f70c791850ca063da99a091ce6bae485dfcbdca
raj1008-git <macof.rj@gmail.com>
2026-08-01 18:51:38 +0530

    Switch Statements

--- Fundamentals/ConditionalStatements/ConditionalStatement.java
+++ Fundamentals/ConditionalStatements/ConditionalStatement.java
@@ -1,13 +1,13 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int x = 8;
-
-        if (x > 10) {
-            System.out.println("Hello");
-        } else {
-            System.out.println("Bye");
-        }
-
+        int result = 0;
+        // if (x % 2 == 0) {
+        // result = 10;
+        // } else {
+        // result = 20;
+        // }
+        result = x % 2 == 0 ? 10 : 20;
+        System.out.println(result);
     }
 }
- 
