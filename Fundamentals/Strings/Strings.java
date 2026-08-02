dd9fd763d9cb90c19c9c680baa2547470c7c272c
raj1008-git <macof.rj@gmail.com>
2026-08-02 19:49:16 +0530

    Revised the string

---
+++ Fundamentals/Strings/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        String name = new String("Raj");
+        System.out.println("Hello" + name);
+        // System.out.println(name.charAt(1));
+        System.out.println(name.concat("Shingh"));
+    }
+}
