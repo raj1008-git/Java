5922f668964a4cc388ac269a3407a48bf99fd5ff
raj1008-git <macof.rj@gmail.com>
2026-08-02 20:26:54 +0530

    Revised about the setters, getters and this keyword

---
+++ Fundamentals/Encapsulation/Encapsulation.java
@@ -0,0 +1,30 @@
+class Human {
+    private int age;
+    private String name;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+  
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Encapsulation {
+    public static void main(String[] args) {
+        Human obj = new Human();
+        obj.setAge(30);
+        obj.setName("Raj");
+        System.out.println(obj.getName());
+        System.out.println(obj.getAge());
+    }
+}
