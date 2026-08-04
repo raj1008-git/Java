ceeed8a6a1300428526cc061f2e674dfbdcb9097
raj1008-git <macof.rj@gmail.com>
2026-08-04 23:44:52 +0530

    Added the enums

--- OOP/Enums.java
+++ OOP/Enums.java
@@ -1,14 +1,32 @@
-enum Status {
-    Running, Failed, Pending, Success
+enum Laptop {
+    Macbook(2000), XPS(5000), Surface(1000), ThinkPad(6000);
+
+    private Laptop() {
+        price = 500;
+    }
+
+    private int price;
+
+    private Laptop(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
 
 public class Enums {
     public static void main(String[] args) {
-        // Status s = Status.Running;
-        Status[] s = Status.values();
-
-        for (Status ss : s) {
-            System.out.println(ss );
+        Laptop lap = Laptop.Macbook;
+        System.out.println(lap + "" + lap.getPrice());
+        for (Laptop laps : Laptop.values()) {
+            System.out.println(lap + ":" + lap.getPrice());
         }
+
     }
 }
