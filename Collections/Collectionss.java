eedfe8851da488dfd1d5ff974706381ee85cb3f8
raj1008-git <macof.rj@gmail.com>
2026-08-11 20:01:41 +0530

    Revised the TreeSet

--- Collections/Collectionss.java
+++ Collections/Collectionss.java
@@ -1,16 +1,23 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Collectionss {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<Integer>();
+        Collection<Integer> nums = new TreeSet<Integer>();
         nums.add(6);
         nums.add(8);
-        System.out.println(nums.indexOf(2));
-        System.out.println(nums.get(1));
-        for (Object n : nums) {
-            System.out.println((int) n);
-        }
+        nums.add(10);
+        nums.add(12);
+        nums.add(13);
+        nums.add(13);
+
+        Iterator<Integer> values = nums.iterator();
+        while (values.hasNext())
+            System.out.println(values.next());
     }
 }
