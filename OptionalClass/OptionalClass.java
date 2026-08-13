bad5631035fd5ec50c330723dc52539c2ecda1b5
raj1008-git <macof.rj@gmail.com>
2026-08-13 20:50:19 +0530

    Revised the Op Class

--- OptionalClass/OptionalClass.java
+++ OptionalClass/OptionalClass.java
@@ -1,12 +1,52 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", age=" + age + "]";
+    }
+}
+
 public class OptionalClass {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Raj", "Singh", "John", "Kishor");
-        Optional<String> name = names.stream().filter(str -> str.contains("x")).findFirst();
-        System.out.println(name.orElse("Not Found"));
+        List<Student> students = new ArrayList<>();
+
+        // for (String name : names) {
+        // students.add(new Student(name));
+        // }
+        students = names.stream().map(name -> new Student(name)).toList();
+        System.out.println(students);
     }
 }
