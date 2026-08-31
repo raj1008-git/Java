93ae07198636db916de36fb463336e99f4badffd
raj1008-git <macof.rj@gmail.com>
2026-08-31 21:14:27 +0530

    Learned the beforeall and after all

---
+++ Gradle/gradle-lesson1/app/src/main/java/org/example/DatabaseConnection.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class DatabaseConnection {
+    void open() {
+        System.out.println("Database Connection Opened");
+    }
+
+    void close() {
+        System.out.println("Database connection closed");
+    }
+}
--- Gradle/gradle-lesson1/app/src/test/java/org/example/AppTest.java
+++ Gradle/gradle-lesson1/app/src/test/java/org/example/AppTest.java
@@ -4,7 +4,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
 class AppTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
+    }
 
     App app;
 
@@ -68,6 +92,21 @@ class AppTest {
 
         int actual = app.divide(10, 2);
         assertEquals(5, actual);
+    }
+
+    @Test
+    void testOne() {
+        System.out.println("Test One");
+    }
+
+    @Test
+    void testTwo() {
+        System.out.println("Test Two");
+    }
+
+    @Test
+    void testThree() {
+        System.out.println("Test Three");
     }
 
 }
