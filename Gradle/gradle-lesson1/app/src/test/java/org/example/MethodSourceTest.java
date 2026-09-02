97af849350b3493ccd2ae839c56b0eaec2f3d246
raj1008-git <macof.rj@gmail.com>
2026-09-02 23:05:42 +0530

    Learned the streams with the junit tests

---
+++ Gradle/gradle-lesson1/app/src/test/java/org/example/MethodSourceTest.java
@@ -0,0 +1,24 @@
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class MethodSourceTest {
+    @ParameterizedTest
+    @MethodSource("attendanceData")
+    void testAttendance(int attendance, boolean expected) {
+        App app = new App();
+        boolean actual = app.canTakeExam(attendance);
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Object[]> attendanceData() {
+        return Stream.of(
+                new Object[] { 75, true },
+                new Object[] { 80, true },
+                new Object[] { 74, false },
+                new Object[] { 50, false });
+    }
+}
